2efc565a1708f94ef28ea492286d0b6757565d8f
eqramhossain <ekram.fr03@gmail.com>
2025-11-21 17:51:31 +0100

    removed printf

--- src/parsing/parsing3.c
+++ src/parsing/parsing3.c
@@ -6,7 +6,7 @@
 /*   By: egerin <egerin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/18 18:12:29 by egerin            #+#    #+#             */
-/*   Updated: 2025/11/19 13:17:17 by egerin           ###   ########.fr       */
+/*   Updated: 2025/11/21 17:51:09 by ehossain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,16 +20,6 @@ int	check_map_file(t_data *data, t_textures *textures)
 		|| !store_textures(data, textures) || !store_rgb(data, textures)
 		|| !check_rgb(textures) || !check_walls(data))
 		return (0);
-	printf("%s\n", textures->no);
-	printf("%s\n", textures->so);
-	printf("%s\n", textures->we);
-	printf("%s\n", textures->ea);
-	printf("%d\n", textures->floor_tab[0]);
-	printf("%d\n", textures->floor_tab[1]);
-	printf("%d\n", textures->floor_tab[2]);
-	printf("%d\n", textures->ceiling_tab[0]);
-	printf("%d\n", textures->ceiling_tab[1]);
-	printf("%d\n", textures->ceiling_tab[2]);
-	printf("ci bon\n");
-	return (1);
+	else
+		return (1);
 }
