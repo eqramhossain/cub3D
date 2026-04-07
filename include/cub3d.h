8d5337d1c08002f73a8bbf66becc5588da2a1892
eqramhossain <ekram.fr03@gmail.com>
2026-04-07 11:51:40 +0200

    Revert "trying to solve map.cub problem"
    This reverts commit 421f20f5998ff53c8eacd0dd8b8da2c4dec8ef6a.

--- include/cub3d.h
+++ include/cub3d.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cub3d.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ekram <ekram@student.42.fr>                +#+  +:+       +#+        */
+/*   By: ehossain <ehossain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/24 12:49:50 by ehossain          #+#    #+#             */
-/*   Updated: 2026/04/07 11:45:48 by ekram            ###   ########.fr       */
+/*   Updated: 2026/04/02 13:13:21 by ehossain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -79,10 +79,6 @@ typedef struct s_ray
 	int				tex_y;
 	double			step;
 	double			tex_pos;
-	int             tex_width;      
-    int             tex_height;
-    char            *tex_addr;
-    int             tex_line_len;
 }					t_ray;
 
 typedef struct s_player
@@ -130,14 +126,6 @@ typedef struct s_texture
 	char			*so_texture;
 	char			*ea_texture;
 	char			*we_texture;
-	int				no_width;
-	int				no_height;
-	int				so_width;
-	int				so_height;
-	int				we_height;
-	int				we_width;
-	int				ea_width;
-	int				ea_height;
 	int				floor;
 	int				ceiling;
 	int				floor_tab[3];
--- src/raycast/ft_raycaster_engine_helper_2.c
+++ src/raycast/ft_raycaster_engine_helper_2.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_raycaster_engine_helper_2.c                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ekram <ekram@student.42.fr>                +#+  +:+       +#+        */
+/*   By: ehossain <ehossain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/29 15:47:14 by ehossain          #+#    #+#             */
-/*   Updated: 2026/04/07 11:45:50 by ekram            ###   ########.fr       */
+/*   Updated: 2026/03/29 15:49:03 by ehossain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,16 @@
 
 void	ft_calculate_texture(t_ray *ray, t_data *data)
 {
-
 	if (ray->side == 0)
 		ray->wall_x = data->player->pos_y + ray->wall_dist * ray->raydir_y;
 	else
 		ray->wall_x = data->player->pos_x + ray->wall_dist * ray->raydir_x;
 	ray->wall_x = ray->wall_x - floor(ray->wall_x);
-	ray->tex_x = (int)(ray->wall_x * (double)(ray->tex_width));
+	ray->tex_x = (int)(ray->wall_x * (double)(WALL_SIZE));
 	if (ray->side == 0 && ray->raydir_x > 0)
-		ray->tex_x = ray->tex_width - ray->tex_x - 1;
+		ray->tex_x = WALL_SIZE - ray->tex_x - 1;
 	if (ray->side == 1 && ray->raydir_y < 0)
-		ray->tex_x = ray->tex_width - ray->tex_x - 1;
+		ray->tex_x = WALL_SIZE - ray->tex_x - 1;
 }
 
 void	ft_get_textures_info(t_ray *ray, t_data *data, char **addr,
